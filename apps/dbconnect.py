fedec83fa0e8be7f7b29785fae662647ef54e72f
Paul Telan <telanpaul@gmail.com>
2023-12-15 12:36:55 +0800

    modified:   apps/dbconnect.py

--- apps/dbconnect.py
+++ apps/dbconnect.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 # Remove comment on your database, comment the rest
 
-def getdblocation():
-    db = psycopg2.connect(
-        host='localhost',
-        database='IE172Project',
-        user='postgres',
-        port=5433,
-        password='36442901'
-    )
-    return db
+# def getdblocation():
+#     db = psycopg2.connect(
+#         host='localhost',
+#         database='IE172Project',
+#         user='postgres',
+#         port=5433,
+#         password='36442901'
+#     )
+#     return db
 
 # def getdblocation():
 #     db = psycopg2.connect(
